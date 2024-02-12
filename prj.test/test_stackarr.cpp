003916f366aaa97c17cb69e32e483153e7a28c94
NemoFlip <artgamer3163@gmail.com>
2024-02-12 20:34:47 +0300

    prj.codeforces structure is completed

--- prj.test/test_stackarr.cpp
+++ prj.test/test_stackarr.cpp
@@ -5,7 +5,7 @@ int main() {
   StackArr stack1 = StackArr();
   stack1.Push(Complex(1.5, 2));
   std::cout << stack1.IsEmpty() << std::endl;
-//  std::cout << stack1.Top() << std::endl;
+  std::cout << stack1.Top() << std::endl;
   std::cout << stack1.GetTopIndex() << std::endl;
   stack1.Pop();
   std::cout << stack1.GetTopIndex() << std::endl;
