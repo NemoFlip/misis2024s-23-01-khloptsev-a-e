383ed5ba83ca78d764826568a56fa84fd63a1899
NemoFlip <artgamer3163@gmail.com>
2024-02-13 13:55:44 +0300

    test_stackarr.cpp is made

--- prj.test/test_stackarr.cpp
+++ prj.test/test_stackarr.cpp
@@ -1,18 +1,52 @@
-#include <iostream>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
 #include <stackarr/stackarr.hpp>
 
-int main() {
-  StackArr stack1 = StackArr();
-  stack1.Push(Complex(1.5, 2));
-  std::cout << stack1.IsEmpty() << std::endl;
-  std::cout << stack1.Top() << std::endl;
-  std::cout << stack1.GetTopIndex() << std::endl;
-  stack1.Pop();
-  std::cout << stack1.GetTopIndex() << std::endl;
-  stack1.Pop();
-  std::cout << stack1.GetTopIndex() << std::endl;
+TEST_CASE("stackarr test") {
+  StackArr my_stack;
+  CHECK_THROWS(my_stack.Top());
+  CHECK(my_stack.IsEmpty());
+  Complex first_complex(3, 5);
+  Complex second_complex(2, 7);
 
+  my_stack.Push(first_complex);
+  my_stack.Push(second_complex);
 
+  CHECK_EQ(my_stack.Top(), second_complex);
+  my_stack.Pop();
+  CHECK_EQ(my_stack.Top(), first_complex);
 
+  Complex third_complex(1, 1);
 
+  StackArr my_stack2;
+  my_stack2.Push(third_complex);
+  StackArr a(my_stack2);
+  CHECK_EQ(a.Top(), third_complex);
+
+  Complex fourth_complex(2.3, 7.3);
+
+  Complex fifth_complex(2.003, 0);
+
+  my_stack.Push(fourth_complex);
+  CHECK_EQ(my_stack.Top(), fourth_complex);
+  my_stack.Push(fifth_complex);
+  CHECK_EQ(my_stack.Top(), fifth_complex);
+
+  StackArr eq1;
+  StackArr eq2;
+  eq1.Push(first_complex);
+  eq1.Push(second_complex);
+  eq1.Push(third_complex);
+  eq2 = eq1;
+  CHECK_EQ(eq2.Top(), third_complex);
+//  eq1.Pop();
+//  eq2.Pop();
+//  CHECK_EQ(eq1.Top(), eq2.Top());
+//  eq1.Pop();
+//  eq2.Pop();
+//  CHECK_EQ(eq1.Top(), eq2.Top());
+//  eq1.Pop();
+//  eq2.Pop();
+//  CHECK(eq1.IsEmpty());
+//  CHECK(eq2.IsEmpty());
 }
