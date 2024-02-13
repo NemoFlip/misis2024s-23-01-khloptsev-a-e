9b8de740b9df2653bfbcf21ee7a07b0391054148
NemoFlip <artgamer3163@gmail.com>
2024-02-13 12:59:03 +0300

    Codeforces task started

---
+++ prj.codeforces/1925a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+int main() {
+  int t = 0;
+  int n = 0;
+  int k = 0;
+  std::cin >> t;
+
+  for (int i = 0; i < t; i += 1) {
+    std::cin >> n >> k; // n * k - длина искомой строки
+    std::string ans;
+
+  }
+
+}
