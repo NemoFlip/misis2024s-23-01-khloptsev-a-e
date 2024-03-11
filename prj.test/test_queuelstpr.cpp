d9ea87d63b2b038674b9f8933123f5611e7100fe
NemoFlip <artgamer3163@gmail.com>
2024-03-11 10:58:32 +0300

    made: QueueLstPr.

--- prj.test/test_queuelstpr.cpp
+++ prj.test/test_queuelstpr.cpp
@@ -36,3 +36,21 @@ TEST_CASE("QueueLstPr ctor tests") {
   queuepr2.Pop();
   CHECK(queuepr2.IsEmpty());
 }
+
+TEST_CASE("QueueLstPr 'operator=' tests") {
+  QueueLstPr queuepr1;
+  float elem1 = 1.0;
+  float elem2 = 2.0;
+  float elem3 = 1.5;
+  queuepr1.Push(elem1);
+  queuepr1.Push(elem2);
+  queuepr1.Push(elem3);
+  QueueLstPr queuepr2 = queuepr1;
+  CHECK_EQ(queuepr2.Top(), elem1);
+  queuepr2.Pop();
+  CHECK_EQ(queuepr2.Top(), elem3);
+  queuepr2.Pop();
+  CHECK_EQ(queuepr2.Top(), elem2);
+  queuepr2.Pop();
+  CHECK(queuepr2.IsEmpty());
+}
