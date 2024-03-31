2c150f71239f98a722ff586b244b8332199e57c0
NemoFlip <artgamer3163@gmail.com>
2024-03-31 20:17:32 +0300

    da

--- prj.lab/bitset/bitset.cpp
+++ prj.lab/bitset/bitset.cpp
@@ -40,6 +40,7 @@ int32_t BitSet::Size() const noexcept {
 // testing git
 //sdfsfjlwjflw
 //fde
+// adf
 void BitSet::Resize(const int32_t size) {
   if (size <= 0) {
     throw std::logic_error("try to resize with negative size");
