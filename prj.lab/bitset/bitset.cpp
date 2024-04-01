a294e6183f825e022c828746d6fde68487e4671b
NemoFlip <artgamer3163@gmail.com>
2024-04-01 21:47:59 +0300

    fix: bitset get and set methods.

--- prj.lab/bitset/bitset.cpp
+++ prj.lab/bitset/bitset.cpp
@@ -2,6 +2,9 @@
 #include <stdexcept>
 // ctors and operator=
 BitSet::BitSet(const std::int32_t size): data_(((size) / 32) + 1, 0), size_ { size } {
+  if (size < 0) {
+    throw std::invalid_argument("Trying to initialize with negative size.");
+  }
 
 }
 BitSet::BitSet(BitSet&& rhs) noexcept {
@@ -52,9 +55,9 @@ void BitSet::Set(const int32_t idx, const bool v) {
   if (size_ <= idx) {
     throw std::out_of_range("Index in out of range");
   }
-  uint32_t one_temp = 1; // 65(2) 31(31) 32(0)
-  uint32_t data_index = data_.size() - 1 - (idx) / 32;
-  one_temp = one_temp << (idx - 32 * (idx / 32));
+  uint32_t one_temp = 1;
+  one_temp = one_temp << (idx % 32);
+  uint32_t data_index = idx / 32; // in that block our bit for idx
   if (v) {
     data_[data_index] = data_[data_index] | one_temp;
   } else {
@@ -64,13 +67,19 @@ void BitSet::Set(const int32_t idx, const bool v) {
 }
 
 bool BitSet::Get(const int32_t idx) const {
-  uint32_t data_index = data_.size() - 1 - (idx) / 32; // index of block in vector
-  uint32_t curr_index = idx - 32 * (idx / 32); // index inside a block in vector
-  uint32_t one_temp = 1;
-  one_temp = one_temp << curr_index;
-  uint32_t result = one_temp & data_[data_index];
-  result = result >> curr_index;
-  return result == 1;
+  if (size_ <= idx) {
+    throw std::logic_error("index is more than size.");
+  }
+  uint32_t current_block = data_[idx / 32]; // in that block our bit for idx
+  uint32_t result = current_block & (1 << (idx % 32));
+  return result != 0;
+//  uint32_t data_index = data_.size() - 1 - (idx) / 32; // index of block in vector
+//  uint32_t curr_index = idx - 32 * (idx / 32); // index inside a block in vector
+//  uint32_t one_temp = 1;
+//  one_temp = one_temp << curr_index;
+//  uint32_t result = one_temp & data_[data_index];
+
+//  return result == 1;
 }
 
 void BitSet::Fill(const bool val) noexcept {
