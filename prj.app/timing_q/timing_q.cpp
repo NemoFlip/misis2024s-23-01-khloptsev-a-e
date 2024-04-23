f4f2bb5a3de6a74259453759e58bdac779b00665
NemoFlip <artgamer3163@gmail.com>
2024-04-23 20:40:31 +0300

    made: prj.app

--- prj.app/timing_q/timing_q.cpp
+++ prj.app/timing_q/timing_q.cpp
@@ -1,7 +1,13 @@
 #include <chrono>
 #include <queuelst/queuelst.hpp>
+#include <iostream>
 
 int main() {
-
+  std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+  QueueLst queuelst1 { };
+  Complex compl1 = {1.1, 1.0};
+  queuelst1.Push(compl1);
+  std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+  std::cout << std::chrono::duration<float>{end - start}.count();
 
 }
